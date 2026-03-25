34648423aed10ac2abf69e0420be815e52d752e0
Mohammad Khan <mohammad@gen7fuel.com>
2026-03-25 15:07:10 -0400

    bol posted shortcut button

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
 import { createAppRouter } from './router'
 
 const queryClient = new QueryClient()
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster richColors />
     </QueryClientProvider>
   </StrictMode>,
 )
