879445edc1454e5a5cbb2bddc5755fff33b0dd22
Mohammad Khan <mohammad@gen7fuel.com>
2026-07-24 10:53:54 -0400

    bugfix npm

--- frontend/src/routes/_appbar/_sidebar/hub/fleet-card-compliance.tsx
+++ frontend/src/routes/_appbar/_sidebar/hub/fleet-card-compliance.tsx
@@ -136,7 +136,6 @@ function RouteComponent() {
 
   useEffect(() => {
     void fetchReport()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [from, to, site])
 
   const totals = useMemo(() => {
